f9bfbc7cb168b72f03bfb22337674efd1448609b
utkutasglk <utku.tasguluk@gmail.com>
2024-11-03 22:44:40 +0300

    Validation done, exception method added for validators

--- accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/example/accounts/dto/AccountsDto.java
@@ -1,15 +1,21 @@
 package com.example.accounts.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType can not be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress can not be null or empty")
     private String branchAddress;
 }
