b7ca7ca0c72dfd5f8bc45fcff82d0a015bf69967
utkutasglk <utku.tasguluk@gmail.com>
2024-11-04 00:03:13 +0300

    Updated audit columns

--- accounts/src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
@@ -35,8 +35,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobile number"
             + customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
 
@@ -51,8 +49,7 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+
         return newAccount;
 
     }
